c92dfddf608f1be979aeb69b0b6a0df678354bc0
Rasindu-Dulshan <rseekkubadu@gmail.com>
2025-07-27 02:03:24 +0530

    done initial frontend configuration

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="flex items-center justify-between p-4 bg-gray-800 text-white"></header>
+  )
+}
+
+export default Navbar
